3e7b6031e137804dd75e2f2b89f9e1c6780cb943
shishir <shishir.chandra@inmobi.com>
2013-11-28 11:15:52 +0530

    maven project

---
+++ FraudClickHeatmap/src/main/java/com/inmobi/com/App.java
@@ -0,0 +1,13 @@
+package com.inmobi.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
